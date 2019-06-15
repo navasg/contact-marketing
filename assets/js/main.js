cbf1e7ecfb4842548f1305df932bf712e43ab724
navasg <genenavas97@gmail.com>
2019-06-15 15:04:51 -0400

    Change dots (carousel)

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,8 @@ $('.slider-nav').slick({
 $('.slick-clients').slick({
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 1,
+  slidesToScroll: 4,
+  dots: true
   // prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
   // nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
 });
